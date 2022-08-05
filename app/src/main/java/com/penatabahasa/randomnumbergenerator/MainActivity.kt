faf040c6fb69bde457374b53d18066bc6f29909e
mjafarshidik <mjafarshidik86@gmail.com>
2022-08-05 20:18:00 +0700

    Layout

--- app/src/main/java/com/penatabahasa/randomnumbergenerator/MainActivity.kt
+++ app/src/main/java/com/penatabahasa/randomnumbergenerator/MainActivity.kt
@@ -7,6 +7,11 @@ import com.penatabahasa.randomnumbergenerator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var Xo : Int = 0
+    private var m : Int = 0
+    private var a : Int = 0
+    private var randomNum : Int = 0
+    private var noOfRandomNum : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,10 +19,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.apply {
-            btnAdditive.setOnClickListener {
-                val intent = Intent(this@MainActivity, AdditiveActivity::class.java)
-                startActivity(intent)
-            }
+
         }
+    }
+
+    fun multiplicativeCongruentialMethod(Xo: Int, m: Int, a: Int, randomNum: Int,noOfRandomNum: Int) : Int{
+        for (i in 1..noOfRandomNum){
+
+        }
+        return Xo
     }
 }
